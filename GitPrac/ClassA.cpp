90d85e97101f79180ea0fd70ab10a03b8549b1aa
Keisuke Matsumura <zokweiron@weiron-iMac.local>
2015-03-19 19:32:30 +0900

    add ClassA

---
+++ GitPrac/ClassA.cpp
@@ -0,0 +1,9 @@
+//
+//  ClassA.cpp
+//  GitPrac
+//
+//  Created by Keisuke Matsumura on 2015/03/19.
+//  Copyright (c) 2015年 Keisuke Matsumura. All rights reserved.
+//
+
+#include "ClassA.h"
---
+++ GitPrac/ClassA.h
@@ -0,0 +1,14 @@
+//
+//  ClassA.h
+//  GitPrac
+//
+//  Created by Keisuke Matsumura on 2015/03/19.
+//  Copyright (c) 2015年 Keisuke Matsumura. All rights reserved.
+//
+
+#ifndef __GitPrac__ClassA__
+#define __GitPrac__ClassA__
+
+#include <stdio.h>
+
+#endif /* defined(__GitPrac__ClassA__) */
